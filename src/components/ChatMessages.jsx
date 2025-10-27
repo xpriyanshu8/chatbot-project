edce97406af17fbb5a37ae95731ff3da60043693
priyanshu kumar <shashimehta07657@gmail.com>
2025-10-27 21:04:22 +0530

    Final commit

---
+++ src/components/ChatMessages.jsx
@@ -0,0 +1,28 @@
+import { useRef, useEffect } from 'react';
+import { ChatMessage } from './ChatMessage';
+import './ChatMessages.css'
+
+function ChatMessages({ chatMessages }) {
+  const chatMessagesRef = useRef(null);
+
+  useEffect(() => {
+    const containerElem = chatMessagesRef.current;
+    if (containerElem) {
+      containerElem.scrollTop = containerElem.scrollHeight;
+    }
+  }, [chatMessages]);
+
+  return (
+    <div className="chat-messages-container" ref={chatMessagesRef}>
+      {chatMessages.map((chatMessage) => (
+        <ChatMessage
+          message={chatMessage.message}
+          sender={chatMessage.sender}
+          key={chatMessage.id}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default ChatMessages;
